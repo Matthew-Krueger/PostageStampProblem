62b2ec976e6bd53e593f99269c5f53790c2ce761
Matthew Krueger <contact@matthewkrueger.com>
2021-12-01 18:25:54 -0500

    Started the basic structure.
    Signed-off-by: Matthew Krueger <contact@matthewkrueger.com>

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PostageStampProblem
+# Postage Stamp  Problem
+Attempts to explain the Postage Stamp Problem also called the Chicken McNuggets Theorem
---
+++ Source/Interface/Interface.cpp
@@ -0,0 +1,52 @@
+/*###########################################################################################################
+#                                                                                                           #
+#    Postage Stamp Problem - Explains the Postage Stamp problem also known as the Chicken McNuggett Theorem #
+#    Copyright (C) 2021 Matthew Krueger <contact@matthewkrueger.com>                                        #
+#                                                                                                           #
+#    This program is free software: you can redistribute it and/or modify                                   #
+#    it under the terms of the GNU General Public License as published by                                   #
+#    the Free Software Foundation, either version 3 of the License, or                                      #
+#    (at your option) any later version.                                                                    #
+#                                                                                                           #
+#    This program is distributed in the hope that it will be useful,                                        #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of                                         #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                          #
+#    GNU General Public License for more details.                                                           #
+#                                                                                                           #
+#    You should have received a copy of the GNU General Public License                                      #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.                                 #
+#                                                                                                           #
+###########################################################################################################*/
+
+#include "Interface.hpp"
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, char** argv){
+
+    /* Print welcome message and give overview of the functionality */
+    {
+        std::cout <<    "Postage Stamp Problem Copyright (C) 2021 Matthew Krueger <contact@matthewkrueger.com>\n\n" <<
+                        "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n" <<
+                        "This is free software, and you are welcome to redistribute it\n"<<
+                        "under certain conditions; type `show c' for details.\n\n\n";
+
+        std::cout <<    "\tThe Postage Problem, otherwise known as the Chicken McNuggett Theorem, is a theory that\n" <<
+                        "given a specific combination of two or more items is reached, such as 11 and 13, that there\n" <<
+                        "exists a number (mn-n-m, where m and n are the two numbers) where every number after that\n" <<
+                        "can be made by a combination of the starting numbers. This program attempts to illustrate that.\n\n\n";
+    }
+
+    while(true){
+
+
+
+    }
+
+    return 0;
+
+}
+
+namespace PostageStampTheorem{
+
+}
